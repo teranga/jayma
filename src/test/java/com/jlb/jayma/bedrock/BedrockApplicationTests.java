58f44c8b8007d2328f468e6b845c8970e05e1e90
Alif <alifb4h@gmail.com>
2025-11-11 17:14:13 -0600

    initial commit

---
+++ src/test/java/com/jlb/jayma/bedrock/BedrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlb.jayma.bedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
